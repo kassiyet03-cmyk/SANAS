d59ea0507bf0bd12c83235da9bedd9421ffeb8b4
Askar-A-A <askar.a.a.dev@gmail.com>
2025-11-22 18:22:46 +0500

    adding whitenose to serve static

--- sanas_project/settings.py
+++ sanas_project/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,6 +122,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'website' / 'static',
 ]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files
 MEDIA_URL = '/media/'
