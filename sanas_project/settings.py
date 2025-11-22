f807ce093f82d436a221974fdc33f45387283453
Askar-A-A <askar.a.a.dev@gmail.com>
2025-11-22 18:03:01 +0500

    adding a template directory

--- sanas_project/settings.py
+++ sanas_project/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'sanas_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'website' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
